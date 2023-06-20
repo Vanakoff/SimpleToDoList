d09cf8284c010d37f62c198fcf09bbfda370c97f
Vanakoff <andrey.vanakoff@gmail.com>
2023-06-20 23:50:21 +0200

    Initial Commit

---
+++ SimpleToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleToDoList
+//
+//  Created by Andrey Vanakoff on 20/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
